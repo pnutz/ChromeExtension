6c0f0d5b3b838b8ad976b76e1d167e2e3454db96
oscarhou <oscarhou@gmail.com>
2014-05-10 00:59:32 -0700

    Draft of Notification ui

--- notification/notificationbar.js
+++ notification/notificationbar.js
@@ -1,3 +1,9 @@
+var data = [ 
+      ["Receipt Item", "Quantity", "Cost"],
+      ["Item1", 1, 423.43],
+      ["Item2", 2, 435]
+    ];
+
 document.addEventListener('DOMContentLoaded', function() {
 	// notification yes
 	$("#yes").click(function()
@@ -15,4 +21,12 @@ document.addEventListener('DOMContentLoaded', function() {
 	{
 		window.parent.postMessage("x", '*');
 	});
+
+  $("#receipt-items").handsontable({
+    data : data,
+    startRows : 4,
+    startCols : 4,
+    stretchH : 'all',
+    width : $("#receipt-items-container").width() 
+  });
 });
