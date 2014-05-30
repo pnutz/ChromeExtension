6ab431b5fc5887499513320a9c1a155b9a0c51c0
oscarhou <oscarhou@gmail.com>
2014-05-29 21:39:23 -0700

    ui improvements to notification bar added functions to modify notification bar

--- notification/notificationbar.js
+++ notification/notificationbar.js
@@ -1,8 +1,74 @@
+var fieldTypes  = 
+{
+  NUMBER : 1 ,
+  TEXT : 2,
+  DATE : 3,
+  SELECT : 4
+
+};
 var NotiBar = 
 {
   configurations:
   {
     itemTableHeaders : ["Receipt Item", "Quantity", "Cost", "Delete"],
+    minSpareRows : 1,
+    formFields : {
+                    "subtotal" : 
+                      {
+                        id : "#receipt-subtotal",
+                        type : fieldTypes.NUMBER
+                      },
+                    "total" : 
+                      {
+                        id : "#receipt-total",
+                        type : fieldTypes.NUMBER
+                      },
+                    "vendor" :
+                      {
+                        id : "#receipt-vendor",
+                        type : fieldTypes.TEXT
+                      },
+                    "address" : 
+                      {
+                        id : "#receipt-vendor-address",
+                        type : fieldTypes.TEXT
+                      },
+                    "invoice" : 
+                      {
+                        id : "#receipt-invoice",
+                        type : fieldTypes.TEXT
+                      },
+                    "taxes" : 
+                      {
+                        id : "#receipt-taxes",
+                        type : fieldTypes.NUMBER
+                      },                    
+                    "notes" : 
+                      {
+                        id : "#receipt-notes",
+                        type : fieldTypes.TEXT
+                      },
+                    "items" : 
+                      {
+                        id : "#receipt-items" ,
+                        type : fieldTypes.TABLE
+                      },                
+                    "profile" : 
+                      {
+                        id : "#receipt-profile",
+                        type : fieldTypes.SELECT
+                      },
+                    "category" :
+                      {
+                        id :  "#receipt-category",
+                        type : fieldTypes.SELECT
+                      },
+                    "date" :
+                      {
+                        id :  "#receipt-date",
+                        type : fieldTypes.DATE
+                      }
+                  }
   },
 
   coverRenderer : function (instance, td, row, col, prop, value, cellProperties) 
@@ -16,7 +82,6 @@ var NotiBar =
     $anchor.attr('row', row);
     $anchor.text('Delete');
     $anchor.click(function() {
-      // Instance is the instance of handsontable
       instance.alter('remove_row', row);
     });
     $(td).empty().append($anchor); //empty is needed because you are rendering to an existing cell
@@ -24,9 +89,13 @@ var NotiBar =
 
   init: function() 
   {
-    this.rowCount = 0;
+    this.rows = [];
+    // set datepicker ui element
+    $(this.configurations.formFields.date.id).datepicker();
+    // set handsontable width to the parent div width
     var tableWidth = $("#receipt-items-container").width() - 100;
     console.log(tableWidth);
+    // setup handsontable for receipt items
     this.receiptItemTable = $("#receipt-items");
     this.receiptItemTable.handsontable({
       stretchH : 'last', //Setting this to 'all' causes resizing issues
@@ -35,31 +104,166 @@ var NotiBar =
       colHeaders : this.configurations.itemTableHeaders,
       rowHeaders : true,
       manualColumnResize : true,
-      minSpareRows :1,
+      minSpareRows : this.configurations.minSpareRows,
+      columnSorting : true,
+      afterCreateRow : function () {
+        console.log("made new row");
+      },
       columns: [
-        {},
-        {},
-        {},
-        {renderer : this.coverRenderer, readOnly : true, copyable: false}
+        {
+          data: 0
+        },
+        {
+          data: 1,
+          type : 'numeric',
+          format : "0.00"
+        },
+        {
+          data: 2,
+          type : 'numeric',
+          format : "$0, 0.00"
+        },
+        {
+          renderer : this.coverRenderer, 
+          readOnly : true, 
+          copyable: false
+        },
       ]
     });
   },
 
-  _addItemRow: function(name, quantity, price)
+  /**
+   * @brief Set entire table data
+   * @param data dictionary of data
+   */
+  setTable: function(data)
   {
-    this.receiptItemTable.handsontable('setDataAtCell', this.rowCount, 0, name);
-    this.receiptItemTable.handsontable('setDataAtCell', this.rowCount, 1, quantity);
-    this.receiptItemTable.handsontable('setDataAtCell', this.rowCount, 2, price);
-    this.rowCount++; 
+    
+  },
+
+  addItemRow: function(name, quantity, price)
+  {
+    var rowNum = this.receiptItemTable.handsontable('countRows') - this.configurations.minSpareRows;
+    this.receiptItemTable.handsontable('setDataAtCell', rowNum, 0, name);
+    this.receiptItemTable.handsontable('setDataAtCell', rowNum, 1, quantity);
+    this.receiptItemTable.handsontable('setDataAtCell', rowNum, 2, price);
   },
   
-  _deleteItemRow: function(index)
+  deleteItemRow: function(rowNum)
   {
-    this.receiptItemTable.handsontable('alter', 'remove_row', index);
+    this.receiptItemTable.handsontable('alter', 'remove_row', rowNum);
+    window.parent.postMessage({
+                                field : 'items',
+                                action : "delete",
+                                index : rowNum 
+                              });
+  },
+
+  getReceiptItems: function()
+  {
+    // Minus one row since there is always an extra empty row
+    var rows = this.receiptItemTable.handsontable('countRows') - this.configurations.minSpareRows;
+    var receiptItems = [];
+    for (var i = 0; i < rows; i++)
+    {
+      var thisRow = this.receiptItemTable.handsontable('getDataAtRow', i);
+      receiptItems.push(thisRow.slice(0,3));
+    }
+    return receiptItems;
+  },
+  
+  setFieldValue: function(fieldName, value)
+  {
+    if (fieldName in this.configurations.formFields)
+    {
+      var field = this.configurations.formFields[fieldName];
+      switch(field.type)
+      {   
+        case fieldTypes.NUMBER:
+        case fieldTypes.TEXT:
+          this.setInputFieldValue(field.id, value);
+          break;
+        case fieldTypes.SELECT:
+          this.setSelectOption(field.id, value);
+          break;
+        default: 
+          console.error("Incorrect type");
+      }
+    }
+    else
+    {
+      console.error("Could not find field : " + fieldName);
+    }
+  },
+
+  /**
+   * @brief set the chosen option for the <select> fields
+   * @params fieldId the id for the element
+   * @params the id for the option
+   */
+  setSelectOption: function(fieldId, optionId)
+  {
+    if (!isNaN(optionId))
+      $(fieldId).val(optionId);
+    else
+      console.error("Expected a numeric value for id");
+  },
+
+  /**
+   * @brief set the text values for the form fields
+   * @params fieldId the id for the element
+   * @params value text value to be shown
+   */
+  setInputFieldValue: function(fieldId, value)
+  {
+    $(fieldId).val(value);
+  },
+
+  /**
+   * @brief set the selected option for <select> fields
+   * @params fieldId the id for the element
+   * @params value the id for the selected option
+   */
+  setSelectFieldOptions: function(fieldName, options)
+  {
+    if (fieldName in this.configurations.formFields &&
+        this.configurations.formFields[fieldName].type == fieldTypes.SELECT)
+    {
+      var field = $(this.configurations.formFields[fieldName].id);
+      $.each(options, function(index, option)
+      {
+        if ("id" in option)
+        {
+          var selectItem = $("<option></option>");
+          selectItem.attr("value", option.id);
+          selectItem.text(option.name);
+          field.append(selectItem);
+        }
+        else
+          console.error("Input option missing id");
+      });
+    }
+    else
+      console.error("Invalid field name" + fieldName);
+  },
+
+  getAllValues: function()
+  {
+    var formDict = {};
+    $.each(this.configurations.formFields, function(key, value)
+    {
+      console.log(key);
+      console.log(value);
+      var formItem = $(value.id); 
+      formDict[formItem.attr('name')] = formItem.val();
+    });
+
+    formDict[$(this.configurations.formFields.items).attr('name')] = this.getReceiptItems();
+    return formDict;
   }
-}
+};
 
 document.addEventListener('DOMContentLoaded', function() {
   NotiBar.init();
-  NotiBar._addItemRow("hello", 3, 43.2);
+  NotiBar.addItemRow("hello", 3, 43.2);
 });
