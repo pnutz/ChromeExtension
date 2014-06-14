e977cb261842bbf906938eabff34cf4c42657913
Oscar Hou <oscarhou@gmail.com>
2014-06-14 14:07:41 -0700

    leftbar now shows user's folders ( doesn't care about levels)

--- vault/vault.js
+++ vault/vault.js
@@ -1,62 +1,100 @@
-var dummyReceipts = ["04/34/2443", "Amazon", "43.5345", "buying stuff"];
 var dummyArray = [];
 var controllers;
 
-// Append credentials to a url
-function appendCred(url)
+
+var Vault = 
 {
-  var credUrl = "";
-  
-  if ("userEmail" in localStorage &&
-      "authToken" in localStorage) {
-    credUrl =  url + 
-        "?email=" + localStorage["userEmail"] + 
-        "&token=" + localStorage["authToken"];
-  } else {
-    console.error("Missing credentials!");
-  }
-  return credUrl;
+  appendCred_: function(url)
+  {
+    var credUrl = "";
+    
+    if ("userEmail" in localStorage &&
+        "authToken" in localStorage) {
+      credUrl =  url + 
+          "?email=" + localStorage["userEmail"] + 
+          "&token=" + localStorage["authToken"];
+    } else {
+      console.error("Missing credentials!");
+    }
+    return credUrl;
+  },
+  init: function ()
+  {
+    // get the controller urls
+    controllers = new ControllerUrls(localStorage["webAppHost"]);
+    // initialize the tab navigation bar
+    $('#vault-navbar a').click(function (e) {
+      e.preventDefault();
+      $(this).tab('show');
+    });
+
+        // If authentication token and email exist then grab receipt data.
+    if ("authToken" in localStorage && "userEmail" in localStorage)
+    {
+      this.getReceipts_();
+      this.getFolders_();
+    }
+    else
+    {
+      console.log("missing credentials");
+    }
+  },
+  getReceipts_: function()
+  {
+    var request = $.ajax({
+          url: this.appendCred_(controllers.GetUrl("receipts") + ".json"),
+          type: 'GET',
+          dataType: 'json'
+        }).done(function(data){
+          console.log("got data" + data[0].vendor_id);
+          $('#vault-receipts').DataTable({
+            "data" :  data,
+            "columns" : [
+              {"data" : "date"},
+              {"data" : "vendor_id"},
+              {"data" : "transaction_number"},
+              {"data" : "total"},
+              {"data" : "note"}
+            ]
+          });
+        }).fail(function (jqXHR, textStatus, errorThrown){
+        // log the error to the console
+          console.error(
+            "The following error occurred: " + textStatus,
+            errorThrown);
+        });
+  },
+  getFolders_: function()
+  {
+    var request = $.ajax({
+          url: this.appendCred_(controllers.GetUrl("folders") + ".json"),
+          type: 'GET',
+          dataType: 'json'
+        }).done(function(data){
+          $.each(data, function(index, value) {
+            console.log("adding " + value.name);
+            var newListItem = $("<li></li>");
+            var newFolder = $("<a></a>")
+            newFolder.text(value.name);
+            newFolder.attr("href", "#vault-receipts-pane");
+            newFolder.attr("data-toggle", "pill");
+            newListItem.append(newFolder);
+            newListItem.insertBefore($("#add-new-folder"));
+          });
+          // Add hook for title change
+          $("#vault-folders-navbar > li > a").click(function(e){
+            $("#folder-name").text(this.text); 
+          });
+        }).fail(function (jqXHR, textStatus, errorThrown){
+        // log the error to the console
+          console.error(
+            "The following error occurred: " + textStatus,
+            errorThrown);
+        });
+  },
 };
 
 document.addEventListener('DOMContentLoaded', function() {
-  controllers = new ControllerUrls(localStorage["webAppHost"]);
-  for (var i = 0; i < 30; i++)
-  {
-    dummyArray.push(dummyReceipts);
-  } 
-  $('#vault-navbar a').click(function (e) {
-    e.preventDefault();
-    $(this).tab('show');
-  });
-  
+  Vault.init();
  
-  if ("authToken" in localStorage && "userEmail" in localStorage)
-  {
-    var request = $.ajax({
-        url: appendCred(controllers.GetUrl("receipts") + ".json"),
-        type: 'GET',
-        dataType: 'json'
-      }).done(function(data){
-        console.log("got data" + data[0].vendor_id);
-        $('#vault-receipts').DataTable({
-          "data" :  data,
-          "columns" : [
-            {"data" : "date"},
-            {"data" : "vendor_id"},
-            {"data" : "transaction_number"},
-            {"data" : "total"},
-            {"data" : "note"}
-          ]
-        });
-      }).fail(function (jqXHR, textStatus, errorThrown){
-      // log the error to the console
-        console.error(
-          "The following error occurred: " + textStatus,
-          errorThrown);
-      });
-  }
-  else
-  {
-    console.log("missing credentials");
-  }
 });
